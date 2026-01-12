c2b2be4a5a8bbaeabad55901db7211b22fc5c2c8
Satoshi00722 <hort6536@gmail.com>
2026-01-12 23:40:55 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-BOT_TOKEN = "8337138670:AAG03YUgXnH_caabtvAU5zrkHfGtcD4mHlY"
+BOT_TOKEN = "8337138670:AAGSjtrM_mwRmceHU6H8D3AyNinX7NMjwtA"
 OPENAI_API_KEY = "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"  # можно оставить пустым
 ADMIN_IDS = [
     6400304134,
     7447763153,
 ]
+
 
 
