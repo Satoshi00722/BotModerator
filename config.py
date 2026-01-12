a4d9fb0215b53c5fbacf73d4cb7ac4997a82ce26
Satoshi00722 <hort6536@gmail.com>
2026-01-12 13:18:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 BOT_TOKEN = "8337138670:AAG03YUgXnH_caabtvAU5zrkHfGtcD4mHlY"
 OPENAI_API_KEY = "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"  # можно оставить пустым
-ADMIN_ID = 7447763153  # твой Telegram ID
+ADMIN_IDS = [
+    6400304134,
+    7447763153,
+]
+
+
